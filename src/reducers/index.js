d046f101c8833076e35aa5e7df213fa1748fb87c
Taylor <stein0209@gmail.com>
2019-11-29 23:51:15 -0500

    adds UserHeader Component and posts and users Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer'
+import usersReducer from  './usersReducer'
 
 export default combineReducers({
-    replaceMe: () => 'baba'
+    posts: postsReducer,
+    users: usersReducer
 });
